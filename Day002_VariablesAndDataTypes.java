588ec2a168e71888a7707552986e1b7680354bed
Shubh Dubey <shubhdubey8756@gmail.com>
2026-01-02 19:30:00 +0530

    Day 2: VariablesAndDataTypes

---
+++ Day002_VariablesAndDataTypes.java
@@ -0,0 +1,27 @@
+/**
+ * ============================================
+ * 120 Days of Java Learning - Day 2
+ * Topic: VariablesAndDataTypes
+ * ============================================
+ * Variable ek naam hai jo memory location ko refer karta hai.
+ * Primitive data types: byte, short, int, long, float, double, char, boolean.
+ * Declaration syntax: dataType variableName = value;
+ */
+public class Day002_VariablesAndDataTypes {
+
+    public static void main(String[] args) {
+
+        int age = 21;
+        double price = 99.99;
+        char grade = 'A';
+        boolean isJavaFun = true;
+        String name = "Shubh";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java Fun: " + isJavaFun);
+    }
+
+}
