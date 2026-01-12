64f52c1eb016b24e268f348ffad5aedd7c934565
Shubh Dubey <shubhdubey8756@gmail.com>
2026-01-12 19:30:00 +0530

    Day 12: WhileLoop

---
+++ Day012_WhileLoop.java
@@ -0,0 +1,20 @@
+/**
+ * ============================================
+ * 120 Days of Java Learning - Day 12
+ * Topic: WhileLoop
+ * ============================================
+ * while loop condition true hone tak repeat hota hai, pehle condition check hoti hai.
+ * Agar condition shuru me hi false ho to loop body kabhi execute nahi hota.
+ */
+public class Day012_WhileLoop {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("While iteration: " + i);
+            i++;
+        }
+    }
+
+}
